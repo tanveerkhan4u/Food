03844f083f0559506776245cfadc75aa1eb762d5
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-05-31 10:39:36 +0530

    update

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
-import food1 from './images/food1.png';
+
 
 
 const Home = () => {
   return (
-   <div>
-    <div className="container-fluid height" >
-    <div className='header'>
-      <h1 className="text-center text-white ">Order Your Favourite Food Here</h1>
-      
+    <div>
+      <div className="container-fluid height" >
+        <div className='header'>
+          <h1 className="text-center text-white texting">Order Your Favourite Food Here</h1>
+          <p className='text-center text-xl shadow-2xl paragrapgh'>Choose From a diverse menu featuring a delectable array of dishes crafted with the finest ingrediants and culinary expertise.</p>
+          <div className="d-flex justify-content-center mt-10">
+          <button1 className="p-5 fs-5 button">
+            search
+          </button1>
+        </div>
+        </div>
+      </div>
     </div>
-    </div>
-   </div>
   )
 }
 
 export default Home;
 
 
-{/* <img className='img-fluid height mx-auto' src={food1} alt="" /> */}
+{/* <img className='img-fluid height mx-auto' src={food1} alt="" /> */ }
 
 
 
