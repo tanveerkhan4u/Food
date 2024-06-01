c543a9e9502e1915a7634505d8ecd577cb5bbc9a
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-06-02 01:35:25 +0530

    update

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -10,7 +10,7 @@ import { EffectCoverflow, Pagination } from 'swiper/modules';
 const Home = () => {
   return (
     <div>
-      <div className="container-fluid height" >
+      <div className="container-fluid height " >
         <div className='header'>
           <h1 className="text-center text-white texting">Order Your Favourite Food Here</h1>
           <p className='text-center text-xl shadow-2xl paragrapgh'>Choose From a diverse menu featuring a delectable array of dishes crafted with the finest ingrediants and culinary expertise.</p>
@@ -23,7 +23,7 @@ const Home = () => {
         </div>
       </div>
       <div className="container">
-        <h3 className='text text-4xl mt-5 ml-5 font-bold'>Explore our menu</h3>
+        <h3 className='text-orange-500 text-4xl mt-5 ml-5 font-bold '>What's on your mind?</h3>
         <p className='text-gray-600 mt-5 menu ml-5 '> choose from diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining  experience one delicious meal at a time.</p>
       </div>
       <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-2 ml-5 mt-5 ">
@@ -63,54 +63,64 @@ const Home = () => {
       </div>
       <br />
       <>
-      <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        pagination={true}
-        modules={[EffectCoverflow, Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <img className='imageswiper w-96 h-96 mx-auto my-auto object-cover ' src="https://images.unsplash.com/photo-1596662951482-0c4ba74a6df6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJ1cmdlcnxlbnwwfHwwfHx8MA%3D%3D" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='imageswiper w-96 h-96 mx-auto my-auto object-cover' src="https://images.unsplash.com/photo-1630431341973-02e1b662ec35?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZyZW5jaCUyMGZyaWVzfGVufDB8fDB8fHww" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='imageswiper w-96 h-96 mx-auto my-auto object-cover' src="https://images.unsplash.com/photo-1628410040883-c412c8d9a0f9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bW9tb3N8ZW58MHx8MHx8fDA%3D" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='imageswiper w-96 h-96 mx-auto my-auto object-cover' src="https://images.unsplash.com/photo-1638439430466-b2bb7fdc1d67?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8fDA%3D" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='imageswiper w-96 h-96 mx-auto my-auto object-cover' src="https://images.unsplash.com/photo-1530469912745-a215c6b256ea?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2hhd2FybWF8ZW58MHx8MHx8fDA%3D" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='imageswiper w-96 h-96 mx-auto my-auto object-cover' src="https://plus.unsplash.com/premium_photo-1674498270805-d1fbcbeef7da?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGZpc2glMjBmb29kfGVufDB8fDB8fHww" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='imageswiper w-96 h-96 mx-auto my-auto object-cover' src="https://images.unsplash.com/photo-1631452180519-c014fe946bc7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGFuZWVyJTIwdGlra2F8ZW58MHx8MHx8fDA%3D" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='imageswiper w-96 h-96 mx-auto my-auto object-cover' src="https://images.unsplash.com/photo-1546793665-c74683f339c1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c2FsYWR8ZW58MHx8MHx8fDA%3D" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='imageswiper w-96 h-96 mx-auto my-auto object-cover' src="https://images.unsplash.com/photo-1579783411194-f697db862dcd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2VhJTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D" />
-        </SwiperSlide>
-        <br/><br/>
-      </Swiper>
+        <Swiper
+          effect={'coverflow'}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={'auto'}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img className='imageswiper w-96 h-96 mx-auto my-auto object-cover px-4 py-4' src="https://images.unsplash.com/photo-1596662951482-0c4ba74a6df6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJ1cmdlcnxlbnwwfHwwfHx8MA%3D%3D" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className='imageswiper w-96 h-96 mx-auto my-auto object-cover px-4 py-4' src="https://images.unsplash.com/photo-1630431341973-02e1b662ec35?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGZyZW5jaCUyMGZyaWVzfGVufDB8fDB8fHww" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className='imageswiper w-96 h-96 mx-auto my-auto object-cover px-4 py-4' src="https://images.unsplash.com/photo-1628410040883-c412c8d9a0f9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bW9tb3N8ZW58MHx8MHx8fDA%3D" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className='imageswiper w-96 h-96 mx-auto my-auto object-cover px-4 py-4' src="https://images.unsplash.com/photo-1638439430466-b2bb7fdc1d67?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8fDA%3D" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className='imageswiper w-96 h-96 mx-auto my-auto object-cover px-4 py-4' src="https://images.unsplash.com/photo-1530469912745-a215c6b256ea?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2hhd2FybWF8ZW58MHx8MHx8fDA%3D" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className='imageswiper w-96 h-96 mx-auto my-auto object-cover px-4 py-4' src="https://plus.unsplash.com/premium_photo-1674498270805-d1fbcbeef7da?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGZpc2glMjBmb29kfGVufDB8fDB8fHww" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className='imageswiper w-96 h-96 mx-auto my-auto object-cover px-4 py-4' src="https://images.unsplash.com/photo-1631452180519-c014fe946bc7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGFuZWVyJTIwdGlra2F8ZW58MHx8MHx8fDA%3D" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className='imageswiper w-96 h-96 mx-auto my-auto object-cover' src="https://images.unsplash.com/photo-1546793665-c74683f339c1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c2FsYWR8ZW58MHx8MHx8fDA%3D" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className='imageswiper w-96 h-96 mx-auto my-auto object-cover' src="https://images.unsplash.com/photo-1579783411194-f697db862dcd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2VhJTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D" />
+          </SwiperSlide>
+          <br /><br />
+        </Swiper>
       </>
-      <br/>
-      
+      <br />
+      <div className="container-fluid  p-5 bg-[url('https://static.vecteezy.com/system/resources/previews/040/986/494/non_2x/ai-generated-cutlery-setting-on-a-restaurant-table-advertising-foodgraphy-photo.jpg')] fork" >
+        <div className="grid grid-cols-1  lg:grid-cols-2 mt-5" >
+          <div>
+            <img src="https://assets.website-files.com/63d0c13bf294b9ad2ad7a1f0/63d9de67f1e5b982041702d6_about-image-01.png " className='object-cover w-full h-96 mx-auto py-4 px-4 ' alt="" />
+          </div>
+          <div className="heading">
+            <h1 className='text-4xl text-center text-orange-500 font-bold'>About Us</h1>
+            <p className='text-gray-600 mt-5 text-lg px-4 py-4  chaku text-white'>We are a team of passionate foodies dedicated to providing you with the best dining experience possible. Our mission is to satisfy your cravings and elevate your dining experience one delicious meal at a time.</p>
+          </div>
+        </div>
+      </div>
 
 
 
