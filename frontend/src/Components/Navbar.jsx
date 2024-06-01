176bcdc2b8b2875aa055381dd2801246254ce9b6
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-06-01 16:47:31 +0530

    update

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -82,26 +82,26 @@ const Navbar = () => {
             {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
             <a
               href="#"
-              className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+              className="bg-gray-900 text-white rounded-md px-3 py-2 mt-4 text-sm font-medium"
               aria-current="page"
             >
               Dashboard
             </a>
             <Link
               to="/Home"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 mt-4 text-sm font-medium"
             >
               Home
             </Link>
             <Link
               to="/Login"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 mt-4 text-sm font-medium"
             >
               Login
             </Link>
             <a
               href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 mt-4 text-sm font-medium"
             >
               Calendar
             </a>
