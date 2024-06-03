d02a3e39ba59ee245d1990867813a9a96783cc19
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-06-03 11:55:47 +0530

    update

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -4,13 +4,16 @@ import { IoCloseOutline } from "react-icons/io5";
 
 
 
+
 const Login = ({ showPopup, setShowPopup }) => {
+  
+  
 
   return (
     <>
-      {!showPopup && (
+      {! showPopup && (
         <div>
-          <div className="h-screen w-scren fixed top-0 left-0 right-0 bg-black/50  z-50 backdrop-blur-sm backdrop-blur-md backdrop-blur-lg">
+          <div className="h-screen w-scren fixed top-0 left-0 right-0 bg-black/30  z-50 backdrop-blur-sm">
             <div className='fixed top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 p-4 shadow-md bg-white rouned-md duration-200 w-[300px]'>
               <div className="flex items-center justify-between">
                 <div>
