3ee128d07cd561059f64a258e72ae32436267a48
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-06-06 23:22:14 +0530

    update

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ import { CiShoppingCart , CiSearch } from "react-icons/ci";
 
 const Navbar = ( ) => {
 
-  const [menu, setmenu] = useState("menu");
+  const [menu, setmenu] = useState("home");
 
 
 
@@ -14,9 +14,9 @@ const Navbar = ( ) => {
   return (
     <>
       <div className='navbar'>
-      <Link to="/Home">
-           <img src={zwigato} className='zwiagto w-20 h-20 object-cover' alt="" />
-           </Link>
+      
+           <img src={zwigato} className='zwiagto  object-cover w-16 h-16' alt="" />
+           
            <ul className="navbar-menu">
             <Link to="/Home" onClick={() =>setmenu("home")} className={menu==="home"?"active":""}>Home</Link>
             <li onClick={() =>setmenu("menu")} className={menu==="menu"?"active":""}>Menu</li>
@@ -24,9 +24,9 @@ const Navbar = ( ) => {
             <li onClick={() =>setmenu("contact us")} className={menu==="contact us"?"active":""}>Contact Us</li>
            </ul>
          <div className='navbar-search'>
-         <CiSearch  className='text-3xl'/>
+         <CiSearch  className='text-3xl chashma'/>
          <div className='navbar-cart'>
-         <CiShoppingCart className='text-3xl'/>
+         <CiShoppingCart className='text-3xl chashma'/>
          <div className='dot'></div>
          </div>
          <button >sign in</button>
