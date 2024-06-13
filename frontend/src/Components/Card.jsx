20f2e179f9b3adac62b9fbb8f30a07fcb1a5f3b1
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-06-13 19:31:35 +0530

    update

--- frontend/src/Components/Card.jsx
+++ frontend/src/Components/Card.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import {AiFillStar} from 'react-icons/ai'
 
-const Card = () => {
+
+function Card  ({food})  {
+  console.log(food);
+  
   return (
-    <div>
-      
+    <div className='font-bold w-[250px] bg-white p-5 flex flex-col rounded-lg gap-2'>
+      <img src={food.img} alt="" className='w-auto h-[150px] hover:scale-110 cursor-grab transition-all duration-500 ease-in-out ' />
+      <div className='text-md flex justify-between mt-2'>
+        <h2>{food.name}</h2>
+        <span className='text-green-500 '>₹{food.price}</span>
+      </div>
+      <p className='text-sm font-normal'>{food.desc}</p>
+      <div className='flex justify-between'>
+        <span className='flex justify-center items-center'>
+        <AiFillStar className='mr-1 text-yellow-400'/> {food.rating}
+        </span>
+        <button className='p-1 text-sm text-white bg-orange-500 rounded-lg hover:bg-orange-600'>Add to cart</button>
+      </div>
     </div>
   )
 }
-
 export default Card
+
--- frontend/src/Components/Menu.jsx
+++ frontend/src/Components/Menu.jsx
@@ -1,33 +1,61 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import Card from './Card'
+import FoodData from './dummydata';
 
 
 
 const Menu = () => {
+
+  const [foodList, setFoodList] = useState(FoodData);
+
+  const searchRef = useRef(null);
+
+  const searchFood = () => {
+    const filteredData = FoodData.filter((food) => {
+      return food.name.toLowerCase().includes(searchRef.current.value.toLowerCase())
+    });
+    setFoodList(filteredData);
+  }
+
+
+
   return (
     <div>
-      <div className="container-fluid mb-10 photo " >
+      <div className="container-fluid  photo " >
         <div className='lg:pt-36 pt-20 '>
           <h1 className='text-center   menu  text-white'>zwigato</h1>
           <p className='text-white text-center text-2xl'>Discover the best food & drinks in Lucknow</p>
         </div>
-      
-          <div className="flex justify-center mt-5 space-x-2 p-4">
+
+        <div className="flex justify-center mt-5 space-x-2 p-4">
           <input
             type="text"
             placeholder="Search..."
-            className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent w-full lg:max-w-xl md:max-w-lg"
+            className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent w-full lg:max-w-xl md:max-w-lg" ref={searchRef}
           />
-          <button1 className="p-2 rounded">
+          <button1 onClick={searchFood} className="p-2 rounded" >
             Search
           </button1>
         </div>
       </div>
     
-
-
+    <div className='container-fluid bg-slate-100 p-5'>
+        <div className='flex flex-wrap gap-10 justify-center mt-5 my-10 ' >
+          {
+            foodList.map((food,name) => {
+              return <Card  food={food}
+             
+              />
+            })
+          }
+        
+        </div>
+        </div>
 
     </div>
   )
 }
 
 export default Menu
+
+// div className=''
